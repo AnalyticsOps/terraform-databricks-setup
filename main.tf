9b4105f169438c00e938ad1ea66c8b3e4916a947
Mikkel Nygaard <mny@energinet.dk>
2022-05-10 14:11:46 +0200

    temp fix

--- main.tf
+++ main.tf
@@ -8,37 +8,37 @@ terraform {
 }
 
 
-provider "databricks" {
-  azure_workspace_resource_id = var.azurerm_databricks_workspace_id
-  azure_client_id             = var.client_id
-  azure_client_secret         = var.client_secret
-  azure_tenant_id             = var.tenant_id
-}
+# provider "databricks" {
+#   azure_workspace_resource_id = var.azurerm_databricks_workspace_id
+#   azure_client_id             = var.client_id
+#   azure_client_secret         = var.client_secret
+#   azure_tenant_id             = var.tenant_id
+# }
 
-resource "databricks_notebook" "ddl" {
-  content_base64 = base64encode(<<-EOT
-    config = {
-      "fs.azure.account.auth.type": "CustomAccessToken",
-      "fs.azure.account.custom.token.provider.class": spark.conf.get("spark.databricks.passthrough.adls.gen2.tokenProviderClassName")
-    }
+# resource "databricks_notebook" "ddl" {
+#   content_base64 = base64encode(<<-EOT
+#     config = {
+#       "fs.azure.account.auth.type": "CustomAccessToken",
+#       "fs.azure.account.custom.token.provider.class": spark.conf.get("spark.databricks.passthrough.adls.gen2.tokenProviderClassName")
+#     }
 
-    dbutils.fs.mount(
-      source = "abfss://datasets@dpcontentstorageprod.dfs.core.windows.net",
-      mount_point = "/mnt/dataplatform",
-      extra_configs = config
-    )
+#     dbutils.fs.mount(
+#       source = "abfss://datasets@dpcontentstorageprod.dfs.core.windows.net",
+#       mount_point = "/mnt/dataplatform",
+#       extra_configs = config
+#     )
 
-    dbutils.fs.mount(
-      source = "abfss://data@${var.storage_account_name}.dfs.core.windows.net",
-      mount_point = "/mnt/data",
-      extra_configs = config
-    )
-    EOT
-  )
-  path = "/Shared/Mount"
-  language = "PYTHON"
+#     dbutils.fs.mount(
+#       source = "abfss://data@${var.storage_account_name}.dfs.core.windows.net",
+#       mount_point = "/mnt/data",
+#       extra_configs = config
+#     )
+#     EOT
+#   )
+#   path = "/Shared/Mount"
+#   language = "PYTHON"
 
-  depends_on = [
-    var.azurerm_databricks_workspace_id
-  ]
-}
+#   depends_on = [
+#     var.azurerm_databricks_workspace_id
+#   ]
+# }
